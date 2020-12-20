92251615aae8ceddcdf7e537372fe149276426f2
João Moreira de Sá Coutinho <joao.moreiradsc@gmail.com>
2020-12-20 20:49:19 +0000

    Add app for lists, with deliberately failing unit test

---
+++ lists/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+# Create your tests here.
+class SmokeTest(TestCase):
+    """Deliberately silly failing test"""
+    def test_bad_maths(self):
+        self.assertEqual(1+1, 3)
